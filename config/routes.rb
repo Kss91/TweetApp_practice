8dcf20bb28e495ebb5c868a8d4fa2e48c2d5764c
Kei <cjbm0901@gmail.com>
2023-07-29 16:54:45 +0900

    HTML CSS　記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/home/top' => "home#top"
+  get '/post/index' => "post/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
