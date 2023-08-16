57fd0f749d600447070a2a753deed2111a1dabc7
Kei <cjbm0901@gmail.com>
2023-08-16 18:38:48 +0900

    create:User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :email, uniquness: true
+  validates :email, uniqueness: true
 
 end
