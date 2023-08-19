74d2423c79a78a1926b0943868feaa97140940fc
Kei <cjbm0901@gmail.com>
2023-08-19 15:49:26 +0900

    create:User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,50 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+    @user  = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice]="ユーザー情報を更新しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice]="ユーザーを作成しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
+  end
+
+  def delete
+    @user  = User.find_by(id: params[:id])
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    flash[:notice]="ユーザーを削除しました"
+    redirect_to("/")
+  end
+
 end
