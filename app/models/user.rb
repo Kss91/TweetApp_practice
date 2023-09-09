35b31d9535a2c32e4e9459048e019d8f89b74789
Kei <cjbm0901@gmail.com>
2023-09-09 12:11:58 +0900

    bug fix:Urerフォロー用に作っていたdependet: :destroyを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id"
 
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
