da003a94f55bb9474b9d3005d77cc14b7059435e
Kei <cjbm0901@gmail.com>
2023-09-09 15:28:41 +0900

    update:html/css修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   before_action :set_current_user
-  before_action :forbid_login_user, {only: [:top]}
+  before_action :forbid_login_user, {only: [:about]}
 
   def top
   end
